8e29f8a508881f5dbe1305b87c2325fbcb357710
idaelsportbet <idaelvargasbusiness@gmail.com>
2026-09-16 21:32:22 -0500

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     }
 
     const precios = {
-  "Premium Diario": "price_1UDCftDLF0zUmGGz5ZXKtc9v",
+ "Premium Diario": "price_1UGV5QDLF0zUmGGzyUgeXdRX",
   "Premium Semanal": "price_1UDCk9DLF0zUmGGzhOLvZdRN",
   "Exclusiva Diaria": "price_1UDCm0DLF0zUmGGzY5gLMWFD",
   "Exclusiva Semanal": "price_1UDCp1DLF0zUmGGzCfpEVDA8",
